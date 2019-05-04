6f20dc26b5b476e4b430501ac3814cac932c6539
Alvaro Almonacid Rojo <alvaro.almonacid.rojo@gmail.com>
2019-05-04 14:00:21 +0200

    Added feature_register

--- app/src/main/java/com/plustraininguk/plustraining/presentation/SplashScreenActivity.kt
+++ app/src/main/java/com/plustraininguk/plustraining/presentation/SplashScreenActivity.kt
@@ -6,11 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 
 import kotlinx.android.synthetic.main.activity_splash.*
 
-class SplashActivity : AppCompatActivity() {
+class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
 
     }
 
