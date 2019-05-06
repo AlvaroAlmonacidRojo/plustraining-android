fc0627af03b32d50995d3c6a26c023eaf2df7a1d
Alvaro Almonacid Rojo <alvaro.almonacid.rojo@gmail.com>
2019-05-06 20:05:07 +0200

    Rename and refactoring buil.gradle.kts(feature_register)

---
+++ feature_register/build.gradle.kts
@@ -0,0 +1,33 @@
+
+
+//apply plugin: 'com.android.application'
+
+plugins{
+    id(GradlePluginId.androidApplication)
+}
+
+android {
+    compileSdkVersion(ApplicationConfig.compileSdkVersion)
+
+
+    defaultConfig {
+        minSdkVersion(ApplicationConfig.minSdkVersion)
+        targetSdkVersion(ApplicationConfig.targetSdkVersion)
+        versionCode = 1
+        versionName = "${ApplicationConfig.versionName}"
+
+        testInstrumentationRunner = ApplicationConfig.testInstrumentationRunner
+
+    }
+
+
+}
+
+dependencies {
+    //implementation fileTree(dir: 'libs', include: ['*.jar'])
+
+    implementation( "com.android.support:appcompat-v7:28.0.0")
+    testImplementation ("junit:junit:4.12")
+    androidTestImplementation ("com.android.support.test:runner:1.0.2")
+    androidTestImplementation ("com.android.support.test.espresso:espresso-core:3.0.2")
+}
